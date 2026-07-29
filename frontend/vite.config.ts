34d42ce541eef566952aee5f8002183363a721c0
Michael James G. Riambon <mikaeljamezz@gmail.com>
2026-07-29 22:04:16 +0800

    chore: adjust chunk size warning limit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   base: "/KUVENTORY/",
   build: {
     sourcemap: false,
+    chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks(id) {
